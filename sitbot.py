7cb35b122ffef4baf9f955a828860ed8615040c6
13werwolf13 <13werwolf13@mail.ru>
2016-04-27 13:08:34 +0600

    Update sitbot.py

--- sitbot.py
+++ sitbot.py
@@ -4,21 +4,14 @@ import pinger
 
 bot = telebot.TeleBot(constants.token)
 
-"""
-if pinger.alert1 == 1:
-    bot.send_message(constants.chatid, pinger.text1)
-if pinger.alert2 == 1:
-    bot.send_message(constants.chatid, pinger.text2)
-if pinger.alert3 == 1:
-    bot.send_message(constants.chatid, pinger.text3)
-if pinger.alert4 == 1:
-    bot.send_message(constants.chatid, pinger.text4)
-"""
+
+@bot.message_handler(commands=['help'])
+def handle_text(message):
+    bot.send_message(constants.chatid, "In order to know the status of servers, use the /PING command.")
+    bot.send_message(constants.chatid, "More bot itself every half hour pings the server and lets you know if one of them fell.")
 
 @bot.message_handler(commands=['ping'])
-
-
-def handle_text (message):
+def handle_text(message):
     pinger.startp()
     bot.send_message(constants.chatid, "pong")
     bot.send_message(constants.chatid, "pong")
