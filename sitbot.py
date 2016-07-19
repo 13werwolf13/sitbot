2643e9a1548a73960809f8d80e77ea9bc90bfb7c
Марков Дмитрий <13werwolf13@mail.ru>
2016-07-19 11:46:19 +0600

    Update sitbot.py

--- sitbot.py
+++ sitbot.py
@@ -7,19 +7,19 @@ bot = telebot.TeleBot(constants.token)
 
 @bot.message_handler(commands=['help'])
 def handle_text(message):
-    bot.send_message(constants.chatid, "In order to know the status of servers, use the /PING command.")
-    bot.send_message(constants.chatid, "More bot itself every half hour pings the server and lets you know if one of them fell.")
+    bot.send_message(message.chat.id, "In order to know the status of servers, use the /PING command.")
+    bot.send_message(message.chat.id, "More bot itself every half hour pings the server and lets you know if one of them fell.")
 
 @bot.message_handler(commands=['ping'])
 def handle_text(message):
     pinger.startp()
-    bot.send_message(constants.chatid, "pong")
-    bot.send_message(constants.chatid, "pong")
-    bot.send_message(constants.chatid, "pong")
-    bot.send_message(constants.chatid, pinger.text1)
-    bot.send_message(constants.chatid, pinger.text2)
-    bot.send_message(constants.chatid, pinger.text3)
-    bot.send_message(constants.chatid, pinger.text4)
+    bot.send_message(message.chat.id, "pong")
+    bot.send_message(message.chat.id, "pong")
+    bot.send_message(message.chat.id, "pong")
+    bot.send_message(message.chat.id, pinger.text1)
+    bot.send_message(message.chat.id, pinger.text2)
+    bot.send_message(message.chat.id, pinger.text3)
+    bot.send_message(message.chat.id, pinger.text4)
 
 
 bot.polling(none_stop=True, interval=0)
